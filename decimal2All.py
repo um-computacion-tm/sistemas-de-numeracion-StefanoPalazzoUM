85a37f266ecd583d376f9f4c61b5916c25a9dec4
Stefano Palazzo <s.palazzo@alumno.um.edu.ar>
2023-04-25 11:20:55 -0300

    Nuevas conversiones

--- decimal2All.py
+++ decimal2All.py
@@ -1,4 +1,5 @@
 import unittest
+from binario2All import binario2hexadecimal, binario2octal
 
 
 def decimal2binario(decimal):
@@ -11,6 +12,13 @@ def decimal2binario(decimal):
     final = final[:: -1]
     return(final)
 
+def decimal2hexadecimal(decimal):
+   binario = decimal2binario(decimal)
+   return(binario2hexadecimal(binario))
+
+def decimal2octal(decimal):
+   binario = decimal2binario(decimal)
+   return(binario2octal(binario))
 
 if __name__ == '__main__':
     unittest.main()
