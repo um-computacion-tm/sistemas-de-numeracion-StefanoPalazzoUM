82b2496a5122d8c23a89fac867e183a03aac6bd9
Stefano Palazzo <s.palazzo@alumno.um.edu.ar>
2023-04-25 10:15:15 -0300

    cambiar nombre

--- binario2All.py
+++ binario2All.py
@@ -15,10 +15,23 @@ def binario2octal(binario):
        digito = int(binario[int(i)])*4 + int(binario[int(i)+1])*2 + int(binario[int(i+2)]) # Le damos peso a cada digito y los guardamos como un digito
        total += str(digito)
     return (total)
+
+def binario2hexadecimal(binario):
+    total = ""                        # Acá se almacena el resultado final
+    while (len(binario) % 4) != 0:    # Agregamos 0s para que se pueda separar en grupos de 4
+        binario = "0" + binario 
+    for i in range(0,len(binario),4): # Separamos en grupos de 4
+       digito = int(binario[int(i)])*8 + int(binario[int(i)+1])*4 + int(binario[int(i+2)])*2 + int(binario[int(i+3)]) # Le damos peso a cada digito y los guardamos como un digito
+       if digito >= 10:
+           AbcHex= "ABCDEF"
+           digito = AbcHex[digito-10]
+       total += str(digito)
+    print (total)
+    return (total)
     
 
 
 binario = input("Ingrese un binario: ")
-binario2octal(binario)
+binario2hexadecimal(binario)
 
 
