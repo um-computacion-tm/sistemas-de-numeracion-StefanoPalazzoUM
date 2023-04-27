2b2734aec8506fbe3705f4da85d6ebe7a76e274f
Stefano Palazzo <s.palazzo@alumno.um.edu.ar>
2023-04-27 11:03:05 -0300

    modificacion  de diccionario

--- Octal2All.py
+++ Octal2All.py
@@ -1,30 +1,35 @@
 import unittest
 from binario2All import  binario2decimal, binario2hexadecimal
 DiccionarioOctal = {
-    '0' : "0000",
-    '1' : "0001",
-    '2' : "0010",
-    '3' : "0011",
-    '4' : "0100",
-    '5' : "0101",
-    '6' : "0110",
-    '7' : "0111",
+    '0' : "000",
+    '1' : "001",
+    '2' : "010",
+    '3' : "011",
+    '4' : "100",
+    '5' : "101",
+    '6' : "110",
+    '7' : "111",
 }
 
 
 def octal2binario(octa):
     octal = ""
+    octa = str(octa)
     for digito in octa:
       octal += str(DiccionarioOctal[digito])
     return(octal)
 
+
 def octal2decimal(octa):
    binario = octal2binario(octa)
-   return(binario2decimal(binario))
+   total=(binario2decimal(binario))
+   return(total)
 
 def octal2hexadecimal(octa):
    binario = octal2binario(octa)
-   return(binario2hexadecimal(binario))
+   final=(binario2hexadecimal(binario))
+   return(final)
+
 
 if __name__ == '__main__':
     unittest.main()
